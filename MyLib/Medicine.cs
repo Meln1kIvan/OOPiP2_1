1884b5bd0ac6f6f203d493825937f7ac771b250e
Meln1kIvan <149098836+Meln1kIvan@users.noreply.github.com>
2024-06-11 22:35:27 +0300

    Add files via upload

---
+++ MyLib/Medicine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    /// <summary>
+    /// Класс лекарств
+    /// </summary>
+    public class Medicine
+    {
+        /// <summary>
+        /// Наименование лекарства
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Лекарственная форма
+        /// </summary>
+        public string Form { get; set; }
+        /// <summary>
+        /// Количество
+        /// </summary>
+        public int Quantity { get; set; }
+        /// <summary>
+        /// Единица измерения
+        /// </summary>
+        public string Unit { get; set; }
+        /// <summary>
+        /// Дозировка
+        /// </summary>
+        public string Dosage { get; set; }
+        /// <summary>
+        /// Цена
+        /// </summary>
+        public decimal Price { get; set; }
+        /// <summary>
+        /// Производитель
+        /// </summary>
+        public string Manufacturer { get; set; }
+    }
+}
