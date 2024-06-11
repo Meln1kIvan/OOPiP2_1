881e41d2e6e713ae25c03a0a9c31ea207aab8fc4
Meln1kIvan <149098836+Meln1kIvan@users.noreply.github.com>
2024-06-11 22:34:44 +0300

    Add files via upload

---
+++ MedicinesDirectory.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MedicinesDirectory.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MedicinesDirectory.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ae1cca18-eb7a-4488-b4b1-d0f5d4f4fff4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MedicinesDirectory.Tests/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using MyLib;
+
+namespace MedicinesDirectory.Tests
+{
+    [TestClass]
+    public class MainWindowTests
+    {
+
+        [TestMethod]
+        public void Medicine_SetProperties_GetPropertiesReturnCorrectValues()
+        {
+            // Arrange
+            var medicine = new Medicine();
+
+            // Act
+            medicine.Name = "Test Medicine";
+            medicine.Form = "Test Form";
+            medicine.Quantity = 10;
+            medicine.Unit = "Test Unit";
+            medicine.Dosage = "Test Dosage";
+            medicine.Price = 9.99m;
+            medicine.Manufacturer = "Test Manufacturer";
+
+            // Assert
+            Assert.AreEqual("Test Medicine", medicine.Name);
+            Assert.AreEqual("Test Form", medicine.Form);
+            Assert.AreEqual(10, medicine.Quantity);
+            Assert.AreEqual("Test Unit", medicine.Unit);
+            Assert.AreEqual("Test Dosage", medicine.Dosage);
+            Assert.AreEqual(9.99m, medicine.Price);
+            Assert.AreEqual("Test Manufacturer", medicine.Manufacturer);
+        }
+
+        [TestMethod]
+        public void Medicine_DefaultConstructor_DefaultPropertyValues()
+        {
+            // Arrange
+            var medicine = new Medicine();
+
+            // Assert
+            Assert.IsNull(medicine.Name);
+            Assert.IsNull(medicine.Form);
+            Assert.AreEqual(0, medicine.Quantity);
+            Assert.IsNull(medicine.Unit);
+            Assert.IsNull(medicine.Dosage);
+            Assert.AreEqual(0m, medicine.Price);
+            Assert.IsNull(medicine.Manufacturer);
+        }
+    }
+}
